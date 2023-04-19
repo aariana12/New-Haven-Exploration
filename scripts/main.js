832a034ce3de47060b98ff96513e3639504e0090
jessiexxcheung <jessiexxcheung@gmail.com>
2023-04-19 16:02:35 -0400

    added test files to see frames within inspect on test.html

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,23 @@
-var socket = new WebSocket("ws://cpsc484-04.yale.internal:8888/frames");
+// var socket = new WebSocket("ws://cpsc484-04.yale.internal:8888/frames");
+
+var host = "cpsc484-04.yale.internal:8888";
+
+$(document).ready(function () {
+    frames.start();
+});
+
+var frames = {
+    socket: null,
+
+    start: function () {
+        var url = "ws://" + host + "/frames";
+        frames.socket = new WebSocket(url);
+        frames.socket.onmessage = function (event) {
+            frames.show(JSON.parse(event.data));
+        }
+    },
+
+    show: function (frame) {
+        console.log(frame);
+    }
+};
