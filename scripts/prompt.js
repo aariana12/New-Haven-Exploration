bfe802ee7f081389117c130968837f3d9de9c7e5
aariana12 <90168628+aariana12@users.noreply.github.com>
2023-04-25 03:05:40 -0400

    Update prompt.js

--- scripts/prompt.js
+++ scripts/prompt.js
@@ -1,10 +1,8 @@
 var host = "localhost:8000";
-let type_page = "study";
 
 // button for restaurants
 let startRes = document.getElementById('restaurant-button');
 startRes.addEventListener("click", () => {
-  type_page = "restaurants";
   window.location.href = '/maps/index.html';
   
 });
@@ -12,7 +10,6 @@ startRes.addEventListener("click", () => {
 // button for study places
 let startStudy = document.getElementById('study-button');
 startStudy.addEventListener("click", () => {
-  type_page = "study";
   window.location.href = '/maps_study/index.html';
   //rename dist map/
   
@@ -21,7 +18,6 @@ startStudy.addEventListener("click", () => {
 // button for hobbies
 let startHobbies = document.getElementById('hobbies-button');
 startHobbies.addEventListener("click", () => {
-  // type_page = "hobbies";
   window.location.href = '/maps_hobbies/index.html';
   
 });
@@ -29,7 +25,6 @@ startHobbies.addEventListener("click", () => {
 // button for places to explore
 let startExplore = document.getElementById('explore-button');
 startExplore.addEventListener("click", () => {
-  type_page = "explore";
   window.location.href = "/maps_explore/index.html";
   
 });
