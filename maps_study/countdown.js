33a80c4878ccd9e2df571bfd50c29ac114304c70
ariana <90168628+aariana12@users.noreply.github.com>
2023-04-25 19:49:18 -0400

    Update countdown.js

--- maps_study/countdown.js
+++ maps_study/countdown.js
@@ -3,7 +3,7 @@
 function countdown() {
     var i = document.getElementById('counter');
     if (parseInt(i.innerHTML)<=0) {
-        location.href = '/qr.html';
+        location.href = '../qr.html';
     }
     if (parseInt(i.innerHTML)!=0) {
         i.innerHTML = parseInt(i.innerHTML)-1;
